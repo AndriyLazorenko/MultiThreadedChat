86ac50356fbd640c47d27bbe4aaba45bc9426429
AndriyLazorenko <andriy.lazorenko@gmail.com>
2015-05-17 11:20:37 +0300

    v 1.2 Messages go to file No Serialize

--- src/Client.java
+++ src/Client.java
@@ -16,6 +16,7 @@ public class Client {
 
         System.out.println("Waiting for server");
         System.out.println(s.isConnected());
+        System.out.println("Type password friend to send objects");
 
         //Client write message
 
@@ -23,14 +24,38 @@ public class Client {
             @Override
             public void run() {
                 try {
-                    PrintWriter pw = new PrintWriter(s.getOutputStream());
-                    Scanner console = new Scanner(System.in);
-                    while (true){
-                        String message = console.nextLine();
-                        pw.println(message);
-                        pw.flush();
+                    while (true) {
+                        //Client has to type password to enter object sending mode
+                        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+                        String password = br.readLine();
+                        //Serialized mode
+                        if (password.equals("friend")) {
+                            System.out.println("Type your message. Type break to exit serialized mode");
+                            ObjectOutputStream oos = new ObjectOutputStream(s.getOutputStream());
+                            Scanner console = new Scanner(System.in);
+                            while (true) {
+                                String message = console.nextLine();
+                                if (message.contains("break")){
+                                    break;
+                                }
+                                else {
+                                    oos.writeObject(message);
+                                    oos.flush();
+                                }
+                            }
+                        }
+                        //Normal mode
+                        else {
+                            PrintWriter pw = new PrintWriter(s.getOutputStream());
+                            Scanner console = new Scanner(System.in);
+                            while (true) {
+                                String message = console.nextLine();
+                                pw.println(message);
+                                pw.flush();
+                            }
+                        }
                     }
-                } catch (IOException e) {
+                }catch(IOException e){
                     e.printStackTrace();
                 }
 
--- src/MyServer.java
+++ src/MyServer.java
@@ -1,8 +1,8 @@
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.LinkedList;
-import java.util.List;
+import java.net.SocketException;
+import java.util.*;
 
 /**
  * Created by Master on 11-May-15.
@@ -16,14 +16,16 @@ public class MyServer {
 
     public static void main(String[] args) throws IOException {
         Socket client;
-        ServerSocket ss = new ServerSocket(10010);
         ClientsContainer container = new ClientsContainer();
+        Properties properties = new Properties();
+        File file = new File("G:\\ServerProperties.txt");
+        FileReader fr = new FileReader(file);
+        properties.load(fr);
+        ServerSocket ss = new ServerSocket(Integer.parseInt(properties.getProperty("port")));
 
-        System.out.println("Waiting for client");
-
-        while (true) {
+        while (container.getNumberOfClients()<Integer.parseInt(properties.getProperty("maxUsersSize"))) {
             client = ss.accept();
-            container.addClient(client.getOutputStream());
+            container.putClient(client,client.getOutputStream());
             ListenerThread lth = new ListenerThread(container,client.getInputStream(),client.getInetAddress().toString(),client.getPort());
             Thread thi = new Thread(lth);
             thi.start();
@@ -54,11 +56,21 @@ public class MyServer {
             while (true){
                 try {
                     String s = bf.readLine();
-                    if(s != null){
+                    if (s != null) {
                         String message = ip + ":" + ":" + port + " -> " + s;
                         observers.sendMessage(message);
                         System.out.println(message);
                     }
+                } catch (SocketException e) {
+                    e.printStackTrace();
+                    Socket forRemoval = null;
+                    for (Socket s:observers.clientSet()){
+                        if (s.getLocalPort()==port){
+                            forRemoval=s;
+                        }
+                    }
+                    observers.removeClient(forRemoval);
+                    break;
                 } catch (IOException e) {
                     e.printStackTrace();
                     break;
@@ -67,50 +79,64 @@ public class MyServer {
 
         }
     }
-    class SpeakerThread implements Runnable{
-        ClientsContainer observer;
-        BufferedReader bf;
-        int port;
-        String ip;
-
-        public SpeakerThread (ClientsContainer clients, InputStream is, String ip, int port){
-            this.observer = clients;
-            this.bf = new BufferedReader(new InputStreamReader(is));
-            this.ip = ip;
-            this.port = port;
-        }
-
-        @Override
-        public void run(){
-            while (true){
-            try {
-                String s = bf.readLine();
-                if(s != null){
-                    String message = ip + ":" + ":" + port + " -> " + s;
-                    observer.sendMessage(message);
-                    System.out.println(message);
-                }
-
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-            }
-        }
-    }
+//    class SpeakerThread implements Runnable{
+//        ClientsContainer observer;
+//        BufferedReader bf;
+//        int port;
+//        String ip;
+//
+//        public SpeakerThread (ClientsContainer clients, InputStream is, String ip, int port){
+//            this.observer = clients;
+//            this.bf = new BufferedReader(new InputStreamReader(is));
+//            this.ip = ip;
+//            this.port = port;
+//        }
+//
+//        @Override
+//        public void run(){
+//            while (true){
+//            try {
+//                String s = bf.readLine();
+//                if(s != null){
+//                    String message = ip + ":" + ":" + port + " -> " + s;
+//                    observer.sendMessage(message);
+//                    System.out.println(message);
+//                }
+//
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            }
+//            }
+//        }
+//    }
 
     class ClientsContainer {
         private File file = new File("G://ChatOutput.txt");
-        public List<PrintWriter> getClients() {
-            return clients;
+//        private List<PrintWriter> clients = new LinkedList<>();
+        private Map<Socket,PrintWriter> clients = new LinkedHashMap<>();
+        private int numberOfClients=0;
+
+        public Set<Socket> clientSet (){
+            return clients.keySet();
+        }
+        public int getNumberOfClients() {
+            setNumberOfClients(clients.size());
+            return numberOfClients;
         }
 
-        private List<PrintWriter> clients = new LinkedList<>();
+        public void setNumberOfClients(int numberOfClients) {
+            this.numberOfClients = numberOfClients;
+        }
 
-        public void addClient (OutputStream os) {
-            this.clients.add(new PrintWriter(os));
+        public void putClient (Socket client, OutputStream os) {
+            this.clients.put(client, new PrintWriter(os));
+        }
+
+        public void removeClient (Socket client) {
+            this.clients.remove(client);
         }
         public synchronized void sendMessage(String message){
-            for (PrintWriter pw : clients){
+            for (PrintWriter pw : clients.values()){
                 pw.println(message);
                 pw.flush();
             }
